a6d4749580f1b499ccb0b06ad2cef637f2a688b2
aymansalem22 <s.aymansalem@gmail.com>
2023-07-24 00:07:24 +0200

    layer of database

---
+++ src/main/java/com/oberla/mediaapp/repository/PlaylistRepository.java
@@ -0,0 +1,40 @@
+package com.oberla.mediaapp.repository;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.oberla.mediaapp.entity.Playlist;
+import com.oberla.mediaapp.entity.Song;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, BigInteger> {
+	
+	public Optional<Playlist> findByName(String name);
+	
+	@Query("select s from Song s where s.playlistId = ?1")
+	public Collection<Song> getSongs(BigInteger playlistId);
+	
+	@Query(value = "select name from song where playlist_id = ?", nativeQuery =true)
+	public List<String> del(BigInteger playlistId);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/oberla/mediaapp/repository/SongRepository.java
@@ -0,0 +1,34 @@
+package com.oberla.mediaapp.repository;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oberla.mediaapp.entity.Song;
+
+public interface SongRepository extends JpaRepository<Song, BigInteger>{
+	
+	public Optional<Song> findByName(String name);
+	
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query("delete from Song s where s.playlistId = ?1")
+	void deleteByPlaylistId(BigInteger playlistId);
+	
+	@Transactional
+	@Modifying(clearAutomatically= true)
+	@Query("delete from Song s where s.playlistId = ?1 and s.id= ?2")
+	public void delete(BigInteger playlistId,BigInteger songId);
+	
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query("update Song s set s.playlistId = ?2 where s.id =?1")
+	public int updatePlaylist(BigInteger songId, BigInteger playlistId);	
+	
+
+
+}
