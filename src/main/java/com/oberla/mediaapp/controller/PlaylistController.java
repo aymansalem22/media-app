f24209b8f0bbef359c3c1b2f7ddc9121be60a948
aymansalem22 <s.aymansalem@gmail.com>
2023-07-24 23:57:20 +0200

    controller rest api

--- src/main/java/com/oberla/mediaapp/controller/PlaylistController.java
+++ src/main/java/com/oberla/mediaapp/controller/PlaylistController.java
@@ -1,16 +1,24 @@
 package com.oberla.mediaapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oberla.mediaapp.service.PlaylistService;
+
 @Controller
-@RequestMapping("/playlist")
 public class PlaylistController {
 	
-	@RequestMapping("/{id}")
-	public String getAllPlayLists(@PathVariable("id") long playlistId) {
-		return "";
+	@Autowired
+	private PlaylistService playlistService;
+	
+	@GetMapping("/")
+	public String listPlaylistAndSongs(Model model) {
+		model.addAttribute("playlists",playlistService.getAllPlaylists());
+		return "index";
 	}
 	
 	
