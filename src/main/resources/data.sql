baac4ebe68696e1d2d921c8988d5bb6c181a1509
aymansalem22 <s.aymansalem@gmail.com>
2023-07-24 23:56:58 +0200

    config data server

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,21 @@
 
-  
-  
-  create table if not exists playlist
-(id bigint auto_increment primary key, name VARCHAR(250)
- NOT NULL, created_on DATE default CURRENT_TIMESTAMP );
+CREATE TABLE IF NOT EXISTS playlist (id bigint AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(250) NOT NULL,
+                                     created_on DATE DEFAULT CURRENT_TIMESTAMP);
 
-create table if not exists song(id bigint 
-auto_increment PRIMARY KEY, playlist_id bigint NOT NULL, 
-name VARCHAR(250) NOT NULL, cover_url VARCHAR(250) NOT NULL, 
-created_on DATE default CURRENT_TIMESTAMP, FOREIGN KEY(playlist_id)
- REFERENCES playlist(id) ON UPDATE CASCADE);
 
-insert into playlist(id, name, created_on) 
-values(1, 'guitar', CURRENT_TIMESTAMP);
+CREATE TABLE IF NOT EXISTS song
+  (id bigint AUTO_INCREMENT PRIMARY KEY,
+   playlist_id bigint NOT NULL,
+   name VARCHAR(250) NOT NULL,
+   cover_url VARCHAR(250) NOT NULL,
+   created_on DATE DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY(playlist_id) REFERENCES playlist(id) ON UPDATE CASCADE);
 
-insert into song(id, name, playlist_id, cover_url, created_on) 
-values(2, 'guitar song', 1, 'https://www.dummies.com/wp-content/uploads/open-guitar-chords-chart.jpg',
- CURRENT_TIMESTAMP);
+
+INSERT INTO playlist(id, name, created_on)
+VALUES(1, 'guitar', CURRENT_TIMESTAMP);
+
+
+INSERT INTO song(id, name, playlist_id, cover_url, created_on)
+VALUES(2, 'guitar song', 1, 'https://www.dummies.com/wp-content/uploads/open-guitar-chords-chart.jpg', CURRENT_TIMESTAMP);
